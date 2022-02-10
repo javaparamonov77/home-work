b29aaa27933cfb07b558a698a069d3086323e4d3
paramonov-yo <paramonov_smfm93@mail.ru>
2022-02-10 21:39:00 +0300

    ДЗ к уроку 18

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -1,6 +1,10 @@
 package com.sbrf.reboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
     }
 }
---
+++ src/main/java/com/sbrf/reboot/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.sbrf.reboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/user")
+    public String getMessage(@RequestParam(value = "name", required = false, defaultValue = "Anonymous") String name, Model model) {
+        model.addAttribute("message", "Hello, " + name + "!");
+        return "hello";
+    }
+}
